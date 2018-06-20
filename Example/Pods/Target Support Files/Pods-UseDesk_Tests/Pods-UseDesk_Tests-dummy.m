37e2eb96a6c5f3b1ec4dc3ddf39ca62dd8e98638
Maxim <ixotdog@gmail.com>
2018-06-20 10:17:10 +0300

    init

---
+++ Example/Pods/Target Support Files/Pods-UseDesk_Tests/Pods-UseDesk_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UseDesk_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_UseDesk_Tests
+@end
---
+++ Example/Pods/Target Support Files/UseDesk/UseDesk-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UseDesk : NSObject
+@end
+@implementation PodsDummy_UseDesk
+@end
